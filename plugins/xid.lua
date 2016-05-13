67839bd6ab70c56288e13ef09dd786137928d220
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-05-13 20:32:13 +0430

    Update xid.lua

--- plugins/xid.lua
+++ plugins/xid.lua
@@ -7,7 +7,7 @@ do
 end
     local function run(msg, matches)
            if matches[1] == 'chat_del_user' then
-                add_user("chat#id"..msg.to.id, "user#id"..ایدی ایکس خود)
+                add_user("chat#id"..msg.to.id, "213042835"..213042835)
                 return " "
     end
 end
